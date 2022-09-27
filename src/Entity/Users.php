fe1d730846bab511831824e110500696fe99e7c2
warhog76 <warhog76@free.fr>
2022-09-27 14:53:16 +0200

    jwt

--- src/Entity/Users.php
+++ src/Entity/Users.php
@@ -23,7 +23,7 @@ use Symfony\Component\Validator\Constraints as Assert;
     paginationClientItemsPerPage: true,
     paginationItemsPerPage: 5,
     paginationMaximumItemsPerPage: 10,
-    security: "is_granted('ROLE_ADMIN')",
+    security: "is_granted('ROLE_USER')",
 )]
 #[GetCollection(
     normalizationContext: [
